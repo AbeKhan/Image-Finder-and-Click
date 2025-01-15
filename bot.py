3fd40c7617c840ba0585443886ff6bcefbecda94
AbeKhan <128449118+AbeKhan@users.noreply.github.com>
2025-01-15 17:50:02 -0600

    Minior Changes

--- bot.py
+++ bot.py
@@ -5,29 +5,29 @@ import keyboard
 import random
 import win32api, win32con
 
-# res = pyautogui.locateOnScreen("edit.png")
-# print(res)
-# shake_but = print(pyautogui.center(res))
+# res = pyautogui.locateOnScreen("edit.png") print(res) shake_but =
+# print(pyautogui.center(res))
 
 # pyautogui.moveTo(shake_but)
 
-def click(x, y):
-    win32api.SetCursorPos((x, y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
-    time.sleep(0.01)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
+#def click(x, y): #  win32api.SetCursorPos((x, y))
+# win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0) #
+# time.sleep(0.01) # win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,
+# 0, 0)
+
+time.sleep(3)   
 while True:
     try:
-        shake_but = pyautogui.locateOnScreen('shake2.png', confidence=0.5, grayscale=True)
+        shake_but = pyautogui.locateOnScreen('shakeText.png', confidence=0.20) #, grayscale=True)
         if shake_but is not None:
+            time.sleep(1)
             pyautogui.moveTo(shake_but)
             time.sleep(1)
-            #click(int(shake_but.left*2), int(shake_but.top/2))
             pyautogui.click()
             time.sleep(1)
             print(shake_but)
-            print("Yes")
+            print("Image Found")
     except pyautogui.ImageNotFoundException:
-        print("No")
+        print("Image Found not found")
         time.sleep(0.5)
